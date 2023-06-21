7fa4721250ccb2bf5dc00e7942947d74ced4570c
AColaluci <a.colaluci@gmail.com>
2023-06-21 13:26:51 -0300

    On branch master  Changes to be committed: 	new file:   src/main/java/com/unla/grupo5OO22023/controllers/LogAuditoriaController.java 	new file:   src/main/java/com/unla/grupo5OO22023/entity/LogAuditoria.java 	modified:   src/main/java/com/unla/grupo5OO22023/helpers/ViewRouteHelper.java 	new file:   src/main/java/com/unla/grupo5OO22023/models/LogAuditoriaModel.java 	new file:   src/main/java/com/unla/grupo5OO22023/repositories/ILogAuditoriaRepository.java 	new file:   src/main/java/com/unla/grupo5OO22023/services/ILogAuditoriaService.java 	modified:   src/main/java/com/unla/grupo5OO22023/services/implementation/DispositivoService.java 	new file:   src/main/java/com/unla/grupo5OO22023/services/implementation/LogAuditoriaService.java

--- src/main/java/com/unla/grupo5OO22023/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/grupo5OO22023/helpers/ViewRouteHelper.java
@@ -2,12 +2,18 @@
 
 public class ViewRouteHelper {
 	
+    public final static String LogAuditoriaIndex = "log/log";
+    public final static String LogAuditoriaRuta = "/log/"; //LogAuditoria
+	
 	//MANDA AL HTML DE INDEX QUE ESTA EN LA CARPETA HOME
 	public final static String INDEX = "home/index";
 		
     public final static String EventoIndex = "evento/evento";
     public final static String EventoRuta = "/evento/"; //Evento
+    
 	
 	public final static String DispositivoIndex = "dispositivo/dispositivos";
 	public final static String DispositivoRuta = "/dispositivo/"; //Dispositivo
+	
+	
 }
