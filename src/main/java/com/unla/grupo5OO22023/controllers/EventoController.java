50fb7dd35f5934034ebb4e61df0f36a1372aa869
Miguel Caraballo <mecf@Miguelecf.local>
2023-06-28 00:41:46 -0300

    Dando retoques a form y abm

--- src/main/java/com/unla/grupo5OO22023/controllers/EventoController.java
+++ src/main/java/com/unla/grupo5OO22023/controllers/EventoController.java
@@ -1,9 +1,16 @@
 package com.unla.grupo5OO22023.controllers;
 
+import com.unla.grupo5OO22023.entity.Dispositivo;
 import com.unla.grupo5OO22023.entity.Evento;
+import com.unla.grupo5OO22023.entity.SensorLuz;
+import com.unla.grupo5OO22023.entity.SensorProximidad;
 import com.unla.grupo5OO22023.helpers.ViewRouteHelper;
 import com.unla.grupo5OO22023.services.IDispositivoService;
 import com.unla.grupo5OO22023.services.IEventoService;
+import com.unla.grupo5OO22023.services.ISensorLuzService;
+import com.unla.grupo5OO22023.services.ISensorProximidadService;
+import com.unla.grupo5OO22023.services.implementation.SensorLuzService;
+
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +26,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/evento")
 public class EventoController {
-	@Autowired
-    @Qualifier("eventoService")
-    private IEventoService eventoService;
 	
+	@Autowired
+	@Qualifier("sensorluzService")
+	private ISensorLuzService sensorluzService;
+
+	@Autowired
+	@Qualifier("sensorProximidadService")
+	private ISensorProximidadService sensorProximidadService;
 	@Autowired
 	@Qualifier("dispositivoService")
 	private IDispositivoService dispositivoService;
+	
+	@Autowired
+    @Qualifier("eventoService")
+    private IEventoService eventoService;
 	
 	@GetMapping("/listar")
 	public String listarEventos(Model model) {
@@ -55,8 +70,12 @@ public class EventoController {
 	
 	@GetMapping("/newevento")
 	public String agregar(Model model) {
+		List<SensorLuz> sensoresLuz = sensorluzService.getAll();
+		List<SensorProximidad> sensoresProximidad = sensorProximidadService.getAll();
 		Evento evento = new Evento();
 		model.addAttribute("evento", evento);
+		model.addAttribute("sensoresLuz",sensoresLuz);
+		model.addAttribute("sensoresProximidad",sensoresProximidad);
 		return ViewRouteHelper.EventoForm;
 	}
 	
